bdeff6f70426be642b93ce74d7365aec45497407
Danielr-1998 <daniel.riano@utp.edu.co>
2024-05-17 20:18:45 -0500

    Primer envio

---
+++ src/test/java/com/market/place/MarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
